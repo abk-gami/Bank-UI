ee05f9790e001558e143243d96eab09a27017e08
abk-gami <yahabubakar01@gmail.com>
2024-01-15 14:48:14 +0100

    e

--- App.js
+++ App.js
@@ -1,12 +1,15 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import { SafeAreaView } from 'react-native-safe-area-context';
 
 export default function App() {
   return (
-    <View style={styles.container}>
+    <SafeAreaView style={styles.container}>
+      <StatusBar style="dark" />
+    <View >
       <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
     </View>
+    </SafeAreaView>
   );
 }
 
@@ -14,7 +17,7 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    // alignItems: 'center',
+    // justifyContent: 'center',
   },
 });
