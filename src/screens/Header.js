8796660da82c4393242f224a3f3b5581df367b8b
Abk <127848306+abk-gami@users.noreply.github.com>
2024-01-18 23:22:14 +0100

    Update Header.js

--- src/screens/Header.js
+++ src/screens/Header.js
@@ -1,7 +1,7 @@
 import { StyleSheet, Text, View, Image } from 'react-native'
 import React from 'react';
 import {BellIcon} from 'react-native-heroicons/outline'
-
+ 
 const Header = () => {
   return (
     <View style={styles.container}>
