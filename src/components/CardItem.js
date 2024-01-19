c38eee48b3789f320d3ed95f08981ba5eb02e8b3
abk-gami <yahabubakar01@gmail.com>
2024-01-19 22:44:32 +0100

    gf

--- src/components/CardItem.js
+++ src/components/CardItem.js
@@ -1,32 +1,71 @@
-import { StyleSheet, Text, View, TouchableOpacity, Dimensions } from 'react-native'
+// import { StyleSheet, Text, View, TouchableOpacity, Dimensions } from 'react-native'
+// import React from 'react'
+
+// const { width, height } = Dimensions.get('window')
+
+// const CardItem = ({image, price, cardType, cardNumber, backgroundColor}) => {
+//   return (
+    
+//     <TouchableOpacity style={styles.buttons}>
+//         <View style={styles.view}>
+
+//       <Text>CardItems</Text>
+//         </View>
+//     </TouchableOpacity>
+    
+//   )
+// }
+
+// export default CardItem
+
+// const styles = StyleSheet.create({
+//     buttons:{
+//         marginRight: 4
+//     },
+//     view: {
+//         paddingVertical: 8,
+//         paddingHorizontal: 4,
+//         justifyContent: 'space-between',
+//         width: width * 0.45,
+//         height: height * 0.3,
+//         backgroundColor: backgroundColor
+//     }
+// })
+
+// import { StyleSheet, Text, View } from 'react-native'
+// import React from 'react'
+
+// export default function CardItem() {
+//   return (
+//     <View>
+//       <Text>CardItem</Text>
+//     </View>
+//   )
+// }
+
+// const styles = StyleSheet.create({
+//     buttons:{
+//         marginRight: 4
+//     },
+//     view: {
+//         paddingVertical: 8,
+//         paddingHorizontal: 4,
+//         justifyContent: 'space-between',
+//         width: width * 0.45,
+//         height: height * 0.3,
+//         backgroundColor: backgroundColor
+//     }
+// })
+
+import { StyleSheet, Text, View } from 'react-native'
 import React from 'react'
 
-const { width, height } = Dimensions.get('window')
-
-const CardItem = () => {
+export default function CardItem({image, price, cardType, cardNumber,backgroundColor}) {
   return (
-    
-    <TouchableOpacity style={styles.buttons}>
-        <View style={styles.view}>
-
-      <Text>CardItems</Text>
-        </View>
-    </TouchableOpacity>
-    
+    <View>
+      <Text style={{backgroundColor: backgroundColor}}>CardItem</Text>
+    </View>
   )
 }
 
-export default CardItem
-
-const styles = StyleSheet.create({
-    buttons:{
-        marginRight: 4
-    },
-    view: {
-        paddingVertical: 8,
-        paddingHorizontal: 4,
-        justifyContent: 'space-between',
-        width: width * 0.45,
-        height: height * 0.3
-    }
-})
+const styles = StyleSheet.create({})
