bd746d63c50df5ac99dc3c44e03c56e60c771535
abk-gami <yahabubakar01@gmail.com>
2024-01-19 22:08:13 +0100

    d

---
+++ src/components/CardItem.js
@@ -0,0 +1,14 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+
+const CardItem = () => {
+  return (
+    <View>
+      <Text>CardItem</Text>
+    </View>
+  )
+}
+
+export default CardItem
+
+const styles = StyleSheet.create({})
