4772f4e8b403578ab12d6b03ff75c1bf9892940f
abk-gami <yahabubakar01@gmail.com>
2024-01-21 11:39:06 +0100

    ds

--- src/screens/Transactions.js
+++ src/screens/Transactions.js
@@ -3,12 +3,20 @@ import React from 'react'
 
 const Transactions = () => {
   return (
-    <View>
-      <Text>Transactions</Text>
+    <View style={styles.container}>
+      <Text style={styles.header}>Transactions</Text>
     </View>
   )
 }
 
 export default Transactions
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({
+  header: {
+    fontSize: 24,
+    fontFamily: 'PoppinsBold'
+  },
+  container: {
+    padding: 10
+  }
+})
