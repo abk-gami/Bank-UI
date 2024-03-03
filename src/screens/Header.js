549597fc883b0a947ef6d73682a974b33b42c2dc
abk-gami <yahabubakar01@gmail.com>
2024-03-03 18:50:25 +0100

    View

--- src/screens/Header.js
+++ src/screens/Header.js
@@ -1,5 +1,5 @@
 import { StyleSheet, Text, View, Image } from 'react-native'
-import React from 'react';
+import React from 'react'; 
 import {BellIcon} from 'react-native-heroicons/outline'
                    
 const Header = () => { 
